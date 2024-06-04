f4c61cec2ee1b73e0db3e0d4e1eb5a8c7b0e9f03
mahesh <kantipudimaheshbabu@gmail.com>
2024-06-04 18:54:53 +0530

    mawabro

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+ import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//import { ThemeProvider } from "@material-tailwind/react";
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <App/>
+   
+      
+    
+    
+
+    
+    
+
+     {/* <App /> */}
+   
+    
   </React.StrictMode>
 );
 
